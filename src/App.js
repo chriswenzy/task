909171745fde5195e37d2a576f44efd0a513371f
Chriswenzy <chrischukwuebuka85@gmail.com>
2022-10-30 08:38:01 +0100

    setup for project routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React from "react";
+import Login from "./pages/authentication/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Register from "./pages/authentication/Register";
+import RegisterConfirm from "./pages/authentication/Register-confirm";
+import Dashboard from "./pages/dashboard/dashboard";
+import UserManagement from "./pages/dashboard/user-management";
+import CreateUser from "./pages/dashboard/user-management/Create-user";
+import EditUser from "./pages/dashboard/user-management/Edit-user";
+import BookManagement from "./pages/dashboard/book-management";
+import CreateBook from "./pages/dashboard/book-management/Create-book";
+import EditBook from "./pages/dashboard/book-management/Edit-book";
+import BookCheckManagement from "./pages/dashboard/book-check-management";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+          <Route exact path="/register-confirm" element={<RegisterConfirm />} />
+          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route exact path="/users" element={<UserManagement />} />
+          <Route exact path="/create-user" element={<CreateUser />} />
+          <Route exact path="/edit-user" element={<EditUser />} />
+          <Route exact path="/books" element={<BookManagement />} />
+          <Route exact path="/create-book" element={<CreateBook />} />
+          <Route exact path="/edit-book" element={<EditBook />} />
+          <Route exact path="/book-check" element={<BookCheckManagement />} />
+        </Routes>
+      </Router>
+    </React.Fragment>
   );
 }
 
